c28271ca42aaca2b4bfe42e793a1bb4a96a6f42b
Harold Mbarga <mbah00@hotmail.com>
2023-08-17 12:37:48 -0400

    modified the if or else script

--- bashshell-scripting/if-else-script.sh
+++ bashshell-scripting/if-else-script.sh
@@ -4,9 +4,9 @@ read -p "Enter the first name: " FIRST_NAME
 read -p "Enter the last name: " LAST_NAME
 read -p "Enter number: " NUMBER
 
-if [[ $NUMBER -gt 10 ]]
+if [[ $NUMBER -gt 18 ]]
 then
-  echo "Hello ${FIRST_NAME} ${LAST_NAME}, The number is greater than 10. "
+  echo "Hello ${FIRST_NAME} ${LAST_NAME}, is greater than 10 Years and as a result can drive a car. "
 else
-  echo "Hello ${FIRST_NAME} ${LAST_NAME}, The number is equal or less than 10. " 
+  echo "Hello ${FIRST_NAME} ${LAST_NAME}, is equal or less than 10 Years as a result cannot drive a car. " 
 fi   
